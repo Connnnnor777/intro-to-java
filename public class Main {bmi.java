75132a98933631c930fc74b9a324a0a269b01741
Connnnnor777 <130242472+Connnnnor777@users.noreply.github.com>
2023-04-08 13:22:25 -0400

    Delete public class Main {bmi.java

--- public class Main {bmi.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-////    Write a program that converts a number entered by a user (1-10) into a Roman numeral.
-////        Ex. 1 on page 267
-////        Note*: Use switch; one class with the main method
-
-
-
-}
